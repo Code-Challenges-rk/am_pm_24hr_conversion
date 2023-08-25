203c00d499fbb95005c36a265d1538f01337a6af
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-08-25 09:41:41 +0300

    refactor: Reformat the JS files.

--- altSolns.js
+++ altSolns.js
@@ -1,12 +1,15 @@
 function convertTo24HourFormat(time) {
-    const date = new Date(`1970-01-01 ${time}`);
-    const formattedTime = date.toLocaleTimeString('en-US', { hour12: false, hour: '2-digit', minute: '2-digit' });
-    // const formattedTime = date.toLocaleTimeString('en-gb');
-    return formattedTime;
-  }
-  
-  // Example usage
-  const inputTime = '02:30 PM';
-  const convertedTime = convertTo24HourFormat(inputTime);
-  console.log(convertedTime);  // Output: "14:30"
-  
+  const date = new Date(`1970-01-01 ${time}`);
+  const formattedTime = date.toLocaleTimeString("en-US", {
+    hour12: false,
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  // const formattedTime = date.toLocaleTimeString('en-gb');
+  return formattedTime;
+}
+
+// Example usage
+const inputTime = "02:30 PM";
+const convertedTime = convertTo24HourFormat(inputTime);
+console.log(convertedTime); // Output: "14:30"
--- soln.js
+++ soln.js
@@ -1,24 +1,25 @@
 function convertTo24HourFormat(time) {
-    // Parse the time string
-    const [timePart, indicator] = time.split(' ');
-    const [hours, minutes] = timePart.split(':').map(Number);
-    
-    // Convert hours to 24-hour format
-    let convertedHours = hours;
-    if (indicator === 'PM' && hours !== 12) {
-      convertedHours += 12;
-    } else if (indicator === 'AM' && hours === 12) {
-      convertedHours = 0;
-    }
-    
-    // Format the time in 24-hour format
-    const formattedTime = `${String(convertedHours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
-    
-    return `${time} => ${formattedTime} hrs`;
+  // Parse the time string
+  const [timePart, indicator] = time.split(" ");
+  const [hours, minutes] = timePart.split(":").map(Number);
+
+  // Convert hours to 24-hour format
+  let convertedHours = hours;
+  if (indicator === "PM" && hours !== 12) {
+    convertedHours += 12;
+  } else if (indicator === "AM" && hours === 12) {
+    convertedHours = 0;
   }
-  
-  // Example usage
-  const inputTime = '12:30 AM';
-  const convertedTime = convertTo24HourFormat(inputTime);
-  console.log(convertedTime);  // Output: "14:30"
-  
+
+  // Format the time in 24-hour format
+  const formattedTime = `${String(convertedHours).padStart(2, "0")}:${String(
+    minutes
+  ).padStart(2, "0")}`;
+
+  return `${time} => ${formattedTime} hrs`;
+}
+
+// Example usage
+const inputTime = "12:30 AM";
+const convertedTime = convertTo24HourFormat(inputTime);
+console.log(convertedTime); // Output: "14:30"
